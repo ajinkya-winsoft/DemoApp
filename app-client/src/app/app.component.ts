e7459c6f9fe8c771ed5808b9249e4c1222b295b9
Ajinkya <ajinkyad@winsoftech.com>
2017-08-11 18:04:05 +0530

    craeted basic directive

--- app-client/src/app/app.component.ts
+++ app-client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { interact } from 'interact.js/interact';
 // import {VisualizationDirective} from './directive/visualization.directive';
-import * as d3 from "d3";
+
 export class Hero {
   id: number;
   name: string;
@@ -17,57 +17,57 @@ export class AppComponent {
     title = 'Sample Application';
 
     ngAfterViewInit() {
-      this.drawGraph();
+     // this.drawGraph();
     }
 
-    drawGraph():void {
-        var svg = d3.select("svg");
-        console.log(svg);
+    // drawGraph():void {
+    //     var svg = d3.select("svg");
+    //     console.log(svg);
 
-         var margin = {top: 20, right: 20, bottom: 30, left: 40},
-          width = +svg.attr("width") - margin.left - margin.right,
-          height = +svg.attr("height") - margin.top - margin.bottom;
+    //      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    //       width = +svg.attr("width") - margin.left - margin.right,
+    //       height = +svg.attr("height") - margin.top - margin.bottom;
 
-      var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-          y = d3.scaleLinear().rangeRound([height, 0]);
+    //   var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    //       y = d3.scaleLinear().rangeRound([height, 0]);
 
-      var g = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //   var g = svg.append("g")
+    //       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      d3.tsv("../assets/data/data.tsv", function(d) {
-        d.frequency = +d.frequency;
-        return d;
-      }, function(error, data) {
-        if (error) throw error;
+    //   d3.tsv("../assets/data/data.tsv", function(d) {
+    //     d.frequency = +d.frequency;
+    //     return d;
+    //   }, function(error, data) {
+    //     if (error) throw error;
 
-        x.domain(data.map(function(d) { return d.letter; }));
-        y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+    //     x.domain(data.map(function(d) { return d.letter; }));
+    //     y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+    //     g.append("g")
+    //         .attr("class", "axis axis--x")
+    //         .attr("transform", "translate(0," + height + ")")
+    //         .call(d3.axisBottom(x));
 
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10, "%"))
-          .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Frequency");
+    //     g.append("g")
+    //         .attr("class", "axis axis--y")
+    //         .call(d3.axisLeft(y).ticks(10, "%"))
+    //       .append("text")
+    //         .attr("transform", "rotate(-90)")
+    //         .attr("y", 6)
+    //         .attr("dy", "0.71em")
+    //         .attr("text-anchor", "end")
+    //         .text("Frequency");
 
-        g.selectAll(".bar")
-          .data(data)
-          .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(d.letter); })
-            .attr("y", function(d) { return y(d.frequency); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return height - y(d.frequency); });
-      });
-    }
+    //     g.selectAll(".bar")
+    //       .data(data)
+    //       .enter().append("rect")
+    //         .attr("class", "bar")
+    //         .attr("x", function(d) { return x(d.letter); })
+    //         .attr("y", function(d) { return y(d.frequency); })
+    //         .attr("width", x.bandwidth())
+    //         .attr("height", function(d) { return height - y(d.frequency); });
+    //   });
+    // }
 
     loadFunc(): void {
         console.log("hello")
--- app-client/src/app/directive/visualization.directive.ts
+++ app-client/src/app/directive/visualization.directive.ts
@@ -1,17 +1,112 @@
-import { Directive, ElementRef, EventEmitter } from '@angular/core';
-
-@Directive({
+import { Directive, ElementRef, EventEmitter, Input, Component } from '@angular/core';
+import * as d3 from "d3";
+@Component({
   selector: 'appVisualization',
+  inputs: ['chartType'],
+  template: `
+      <svg width="960" height="500"  class="draggable"></svg>
+    `
 })
 export class VisualizationDirective {
   loadFunc = new EventEmitter();
-  constructor() {
-      console.log("directive loaded");
-      setInterval(() => this.loadFunc.emit("event"), 1000);
+
+  constructor(private el: ElementRef) {
+    console.log("directive loaded");
   }
 
-  getProperty():void {
-      console.log("directive function called")
+  visualtizationType: string;
+
+  @Input('chart-type') chartType: string;
+
+  ngOnInit() {
+    this.visualtizationType = this.getVisualizationType(this.chartType);
+    if (this.visualtizationType == undefined) {
+      throw new Error("Undefined chart type");
+    } else {
+      let chart = new BarChart(this.visualtizationType);
+      chart.drawGraph();
+    }
+  }
+
+
+  getVisualizationType(type: string): string {
+    console.log(type)
+    let visualtizationType: string;
+    switch (type) {
+      case "Barchart": {
+        this.visualtizationType = "Barchart";
+        break;
+      }
+
+      default: {
+        this.visualtizationType = undefined;
+        break;
+      }
+    }
+    return this.visualtizationType;
+  }
+
+  getProperty(): void {
+    console.log("directive function called");
+  }
+
+}
+
+export class Chart {
+  visualtizationType: String;
+  constructor(visualtizationType: string) {
+    this.visualtizationType = visualtizationType;
+  }
+}
+
+export class BarChart extends Chart {
+  drawGraph(): void {
+    var svg = d3.select("svg");
+    console.log(svg);
+
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
+
+    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+    var g = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.tsv("../assets/data/data.tsv", function (d) {
+      d.frequency = +d.frequency;
+      return d;
+    }, function (error, data) {
+      if (error) throw error;
+
+      x.domain(data.map(function (d) { return d.letter; }));
+      y.domain([0, d3.max(data, function (d) { return d.frequency; })]);
+
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
+
+      g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.letter); })
+        .attr("y", function (d) { return y(d.frequency); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d.frequency); });
+    });
   }
 
 }
